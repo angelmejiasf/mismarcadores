90dd16dbd628b8de7d57f5eede8c8505cd422b59
angelmejiasf <angelmejias@gmail.com>
2023-11-23 18:46:33 +0100

    modificacion de algunos estilos

--- partidos.php
+++ partidos.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MisMarcadores</title>
+        <title>MisMarcadores-Usuario</title>
         <link rel="icon" href="./assets/images/mis-marcadores-1.ico" type="image/x-icon">
         <link rel="shortcut icon" href="./assets/mis-marcadores-1.ico" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="css/estilo.css" />
@@ -11,8 +11,11 @@
     <body>
         <div class="container">
             <h1 class="title">Mis Marcadores</h1><img src="./assets/images/mis-marcadores-1.ico" width="100px"/><br>
-            <?php
             
+          
+            <?php
+            //Pagina para usuarios
+            //----------------DATOS BUNDESLIGA--------------
             if(isset($_POST['bundesliga'])){
             //Conexion Base de Datos
             $cadena_conexion = "mysql:dbname=mismarcadores;host=127.0.0.1";
@@ -49,18 +52,104 @@
 
                 echo "</table>";
 
-                /* foreach ($resultados as $fila) {
-                  // Accede a los datos de cada fila usando $fila['nombre_de_columna']
-                  echo $fila['equipolocal']." " . $fila['resultadolocal'] . "-" .$fila['resultadovisitante']. " ".$fila['equipovisitante']."<br>";
-                  } */
+               
             } catch (Exception $e) {
                 echo "Error con la bd: " . $e->getMessage();
             }
             }
+            
+            //---------------DATOS PREMIER LEAGUE---------------
+            if(isset($_POST['premier'])){
+            //Conexion Base de Datos
+            $cadena_conexion = "mysql:dbname=mismarcadores;host=127.0.0.1";
+            $usuario = "root";
+            $clave = "";
+
+            try {
+                $bd = new PDO($cadena_conexion, $usuario, $clave);
+
+                $consulta = $bd->prepare("SELECT * FROM premierleague");
+                $consulta->execute();
+
+                // Obtiene la información de las columnas
+                $columnas = $consulta->getColumnMeta(0);
+
+                // Obtiene el nombre de la tabla
+                $nombre_tabla = $columnas['table'];
+
+                // Vuelve a ejecutar la consulta para obtener los resultados
+                $consulta->execute();
+                $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+                
+                echo "<p class='competicion'>Competicion: " . $nombre_tabla ."</p>";
+                echo "<table border='1' class='table'>";
+                foreach ($resultados as $fila) {
+
+                    echo "<tr>";
+                    echo "<td>" . $fila['equipolocal'] . "</td>";
+                    echo "<td>" . $fila['resultadolocal'] . "</td>";
+                    echo "<td>" . $fila['resultadovisitante'] . "</td>";
+                    echo "<td>" . $fila['equipovisitante'] . "</td>";
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+
+               
+            } catch (Exception $e) {
+                echo "Error con la bd: " . $e->getMessage();
+            }
+            }
+            
+            //-------------DATOS LALIGA---------------
+             if(isset($_POST['laliga'])){
+            //Conexion Base de Datos
+            $cadena_conexion = "mysql:dbname=mismarcadores;host=127.0.0.1";
+            $usuario = "root";
+            $clave = "";
+
+            try {
+                $bd = new PDO($cadena_conexion, $usuario, $clave);
+
+                $consulta = $bd->prepare("SELECT * FROM laligaeasports");
+                $consulta->execute();
+
+                // Obtiene la información de las columnas
+                $columnas = $consulta->getColumnMeta(0);
+
+                // Obtiene el nombre de la tabla
+                $nombre_tabla = $columnas['table'];
+
+                // Vuelve a ejecutar la consulta para obtener los resultados
+                $consulta->execute();
+                $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+                
+                echo "<p class='competicion'>Competicion:. $nombre_tabla</p>";
+                echo "<table border='1' class='table'>";
+                foreach ($resultados as $fila) {
+
+                    echo "<tr>";
+                    echo "<td>" . $fila['equipolocal'] . "</td>";
+                    echo "<td>" . $fila['resultadolocal'] . "</td>";
+                    echo "<td>" . $fila['resultadovisitante'] . "</td>";
+                    echo "<td>" . $fila['equipovisitante'] . "</td>";
+                    echo "</tr>";
+                }
+
+                echo "</table>";
+
+               
+            } catch (Exception $e) {
+                echo "Error con la bd: " . $e->getMessage();
+            }
+            }
+            
             ?>
             
              <form action="partidos.php" method="post">
         <input type="submit" name="bundesliga" value="Bundesliga">
+        <input type="submit" name="premier" value="Premier League">
+        <input type="submit" name="laliga" value="La Liga EASPORTS">
                
     </form>
         </div>
