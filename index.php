af7c0933b6c67bc95e72e2c68e1d2092f6e0a39f
angelmejiasf <angelmejias@gmail.com>
2023-11-30 18:48:45 +0100

    login y regiter insertado

--- index.php
+++ index.php
@@ -6,26 +6,66 @@
         <title>MisMarcadores</title>
         <link rel="icon" href="./assets/images/mis-marcadores-1.ico" type="image/x-icon">
         <link rel="shortcut icon" href="./assets/images/mis-marcadores-1.ico" type="image/x-icon">
-        <link rel="stylesheet" type="text/css" href="css/estilo.css" />
+        <link rel="stylesheet" type="text/css" href="css/styleindex.css" />
+
     </head>
     <body>
-    <div class="container">
-        <h1 class="title">Mis Marcadores </h1><img src="./assets/images/mis-marcadores-1.ico" width="100px"/><br>
-        <section class="section">
-            <article>
-                <h1>Inicia Sesion</h1>
+        <div class="container">
+            <h1 class="title">Mis Marcadores </h1><img src="./assets/images/mis-marcadores-1.ico" width="100px"/><br>
+            <section class="section">
+                <article>
+
+                    <form action="index.php" method="post">
+
+                        <h1>Iniciar Sesion</h1>
+
+
+                        <label for="username">Usuario</label>
+                        <input type="text" id="username" name="username" required>
+
+                        <label for="password">Contraseña</label>
+                        <input type="password" id="password" name="password" required>
+
+                        <button type="submit">Iniciar sesión</button>
+                    </form>
+                    <p>Si no estas registrado.<a href="./pages/register.php" class="href">Registrate</a></p>
+                </article>
+
+                <?php
+                session_start();
                 
-            </article>
-            
-        </section>
-        <?php
-        //Iniciar Sesion
-        
-        
-        ?>
-        
-        <a href="partidos.php">partidos</a>
-    </div>
+                // Verificar si el usuario ya está autenticado
+                if (isset($_SESSION['usuario'])) {
+                    header("Location: index.php");
+                    exit();
+                }
+
+                // Verificar si se ha enviado el formulario
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $usuario = $_POST["username"];
+                    $contrasena = $_POST["password"];
+
+                    // Verificar si es el admin
+                    if ($usuario == "admin" && $contrasena == "admin") {
+                        header("Location: ./pages/partidosadmin.php");
+                        exit();
+                    }
+                    // Verificar si es un usuario normal
+                    elseif ($usuario == "user" && $contrasena == "user") {
+                        header("Location: ./pages/partidos.php");
+                        exit();
+                    } else {
+                        $error = "Usuario o contraseña incorrecta";
+                        echo '<p class="error">' . $error . '</p>';
+                    }
+                }
+                ?>
+
+            </section>
+
+
+
+        </div>
     </body>
 </html>
 
---
+++ pages/partidosadmin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MisMarcadores-Usuario</title>
+        <link rel="icon" href="./assets/images/mis-marcadores-1.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="./assets/mis-marcadores-1.ico" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="../css/estilo.css" />
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="title">Mis Marcadores</h1>
+            <a href="../index.php"><img src="../assets/images/mis-marcadores-1.ico" width="100px"/></a><br>
+        
+        
+        <section class="section">
+        <a href="./insertar.php">Insertar Partidos</a>
+        <a href="./eliminar.php">Eliminar Partidos</a>
+        <a href="./actualizar.php">Actualizar Partidos</a>
+        </section>
+        
+  
+
+
+
+</div>
+    </body>
+</html>
